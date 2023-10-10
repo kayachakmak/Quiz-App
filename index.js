9c722f2cc6789a54240317a0d73c156897099ca6
Kaya Chakmak <kycakmak@gmail.com>
2023-10-10 10:27:03 +0200

    add functionality to bookmark button

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const darkMode = document.querySelector('[data-js="dark_mode"]');
 const main = document.querySelector('[data-js="main"]');
 
-darkMode.addEventListener("click", () => {
-  main.classList.toggle("dark");
-  document.cookie =
-    "darkMode=on; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
+// darkMode.addEventListener("click", () => {
+//   main.classList.toggle("dark");
+// });
+
+const filled_bookmark = document.querySelector('[data-js="image_filled"]');
+const empty_bookmark = document.querySelector('[data-js="image_empty"]');
+toggleButton = document.querySelector("#questions__container__button-bookmark");
+
+toggleButton.addEventListener("click", () => {
+  filled_bookmark.classList.toggle("hidden");
+  empty_bookmark.classList.toggle("hidden");
 });
